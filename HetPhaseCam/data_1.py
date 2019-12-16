b09c86fd4d39ecc261ae653e15e9489e2463eb1c
KadenJLoring <35496232+KadenJLoring@users.noreply.github.com>
2019-12-16 18:47:20 -0500

    New version of data_1 with more automation
    This version no longer requires user to know the number of numpy arrays in the folder which their data is saved. Furthermore, the data is more modular, having each data run from save_acquire saved to a different folder. The phase map generated in this script also has a time stamp now in case multiple phase maps of the same data are ever made.

--- HetPhaseCam/data_1.py
+++ HetPhaseCam/data_1.py
@@ -1,80 +1,102 @@
-#using weird numpy arrays\
+#using numpy arrays
 import os
 import PySpin
 import numpy
 import matplotlib.pyplot as plt
 import time
+import datetime
 from PIL import Image
 from numpy import array, empty, ravel, where, ones, reshape, arctan2
 from matplotlib.pyplot import plot, draw, show, ion
+from datetime import date
 
-#read in some number of images 
-File_Name = input("Enter the name of the file you wish to retrieve the images from: ") #File_Name in save_aquire 
-NUM_IMAGES = int(input("Please enter the number of images in the file: "))
+
+#User input of which Data folder they desire
+Folder_Name = input("Enter the name of the file you wish to retrieve the images from: ") #Folder_Name in some previous save_aquire (versions newer than 191216)
+
+#Find number of numpy array files within 'Folder_Name'
+Data_dir = 'C:/Users/localadmin/Desktop/Phase_Camera_Images/Data/'
+data_path = os.path.join(Data_dir,Folder_Name)
+NUM_IMAGES_list = os.listdir(data_path) #Num(instensity images)=Num(numpy arrays), not the number of phase maps
+NUM_IMAGES = len(NUM_IMAGES_list)
+print("Number of intensity image numpy arrays present in " + Folder_Name + " is: ", NUM_IMAGES)
 
 picList = []
 NOVAK_PHA_PLT =[]	
 
 #open image arrays 
 for i in range(NUM_IMAGES):
-	imgName = 'C:/Users/localadmin/Desktop/Phase_Camera_Images/Data/' + File_Name + '-%d.npy' % i
+	imgName = data_path +"/" + Folder_Name + '_%d.npy' % i
 	IMG = numpy.load(imgName)
 	picList.append(IMG)
 	
 		
 #average carre phase map
-carre_avg = numpy.empty(388800)
+FPP_avg = numpy.empty(388800)
 carre_no_Mask = numpy.empty(388800) #maybe later. compare to mask
 c = 0
 
 for i in range(NUM_IMAGES):	
-	if i % 4 == 0 and i > 0:
+	if i % 8 == 0 and i > 0:  #MAKE SURE OVERLAP IS RIGHT... (%8 FOR NOVAK, %4 FOR 4P AND CARRE)
 		c += 1
-		arr1 = numpy.ravel(numpy.array(picList[i],dtype='int'))
-		arr2 = numpy.ravel(numpy.array(picList[i-1],dtype='int')) #converts to numpy arrays for faster operation
-		arr3 = numpy.ravel(numpy.array(picList[i-2],dtype='int'))
-		arr4 = numpy.ravel(numpy.array(picList[i-3],dtype='int'))
-
+		#phase of each pixel, assuming: list of five images read in, equally centered and sized
+		arr1 = numpy.ravel(numpy.array(picList[0],dtype='int'))
+		arr2 = numpy.ravel(numpy.array(picList[1],dtype='int')) #converts to numpy arrays for faster operation
+		arr3 = numpy.ravel(numpy.array(picList[2],dtype='int'))
+		arr4 = numpy.ravel(numpy.array(picList[3],dtype='int'))
+		arr5 = numpy.ravel(numpy.array(picList[4],dtype='int'))
 		phase = numpy.empty(388800)
 
 		mask = numpy.ones(388800,dtype=bool)
-		 
+
 		cuts = numpy.where(arr1 < 15)
 
 		mask[cuts] = False
 
-		p1 = arr1#[mask]
-		p2 = arr2#[mask]
-		p3 = arr3#[mask]
-		p4 = arr4#[mask]
-				
-		B = p1-p4
-		A = p2-p3 
-		num = (A+B) * (3*A-B)
-		num = numpy.sqrt(abs(num))
+		p1 = arr1[mask]
+		p2 = arr2[mask]
+		p3 = arr3[mask]
+		p4 = arr4[mask]
+		p5 = arr5[mask]	
+
+		den = 2*p3-p1-p5
+
+		A = p2-p4
+
+		B = p1-p5
+
+		num = numpy.sqrt(abs(4*A**2-B**2))
+
 		pm = numpy.sign(A)
-		den = p2 + p3 - p1 - p4 
+
 		pha = numpy.arctan2(pm*num,den)
-		#phase[~mask] = 0
-		#phase[mask] = pha
+
+		phase[~mask] = 0
+		phase[mask] = pha
 		
-		carre_avg += pha#se
+		FPP_avg += phase
 
 	
-carre_avg = carre_avg / c
-carre_avg = numpy.reshape(carre_avg,(540,720))
+FPP_avg = FPP_avg / c 
+FPP_avg = numpy.reshape(FPP_avg,(540,720))
 
 plt.ion()						
-plt.imshow(carre_avg, cmap = 'jet')
+plt.imshow(FPP_avg, cmap = 'jet')
 cbar = plt.colorbar()#
 plt.clim(vmin=-numpy.pi,vmax=numpy.pi)#
 cbar.set_label("Phase Shift (rad)")#
 plt.xlabel("Pixels(x)")
 plt.ylabel("Pixels(y)")
-#plt.xlim([300,500])
-#plt.ylim([200,400])
+plt.xlim([250,500])
+plt.ylim([450,200])
 plt.pause(0.00001)
-name = 'C:/Users/localadmin/Desktop/Phase_Camera_Images/' + File_Name + '_carre_no_Mask.png' #it's an average of ten so not mega
-plt.savefig(name)
+
+today=date.today()
+d1 = today.strftime("%Y%m%d")	
+
+phase_map_name = 'C:/Users/localadmin/Desktop/Phase_Camera_Images/phase_maps/' + Folder_Name + '_' + d1  + '_Novak.png' 
+plt.savefig(phase_map_name)
 plt.show()
 plt.clf()
+
+print("phase map saved at:", phase_map_name)
